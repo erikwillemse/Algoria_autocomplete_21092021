8b4676fadfab3480e5a325d33429a690f10288a4
erikwillemse <47789869+erikwillemse@users.noreply.github.com>
2021-09-20 11:13:56 +0200

    edit title

--- app.tsx
+++ app.tsx
@@ -1,26 +1,26 @@
-import { autocomplete } from '@algolia/autocomplete-js';
-import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
-import algoliasearch from 'algoliasearch';
+import { autocomplete } from "@algolia/autocomplete-js";
+import { createQuerySuggestionsPlugin } from "@algolia/autocomplete-plugin-query-suggestions";
+import algoliasearch from "algoliasearch";
 
-import '@algolia/autocomplete-theme-classic';
+import "@algolia/autocomplete-theme-classic";
 
-const appId = 'CSDBX0SZMQ';
-const apiKey = '4bfa904cde10c4036e72bb5ad6a698d4';
+const appId = "CSDBX0SZMQ";
+const apiKey = "4bfa904cde10c4036e72bb5ad6a698d4";
 const searchClient = algoliasearch(appId, apiKey);
 
 const querySuggestionsPlugin = createQuerySuggestionsPlugin({
   searchClient,
-  indexName: 'crawler_ta_content',
+  indexName: "crawler_ta_content",
   getSearchParams() {
     return {
-      hitsPerPage: 10,
+      hitsPerPage: 10
     };
-  },
+  }
 });
 
 autocomplete({
-  container: '#autocomplete',
-  placeholder: 'Search',
+  container: "#autocomplete",
+  placeholder: "Search",
   openOnFocus: true,
-  plugins: [querySuggestionsPlugin],
+  plugins: [querySuggestionsPlugin]
 });
